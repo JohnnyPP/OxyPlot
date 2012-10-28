fcf64be53312c1baef9c2ba3ac0f14a4e985a3ff
JohnnyPP <johnnnyppp@gmail.com>
2012-10-28 15:17:50 +0100

    Serial port, adjusted plot properties

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,52 +36,71 @@ namespace WindowsFormsDemo
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.plot1 = new OxyPlot.WindowsForms.Plot();
-            this.button1 = new System.Windows.Forms.Button();
+            this.button1_Connect = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button2_Disconnect = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // plot1
             // 
-            this.plot1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.plot1.KeyboardPanHorizontalStep = 0.1D;
             this.plot1.KeyboardPanVerticalStep = 0.1D;
-            this.plot1.Location = new System.Drawing.Point(0, 0);
+            this.plot1.Location = new System.Drawing.Point(173, 12);
             this.plot1.Margin = new System.Windows.Forms.Padding(0);
             this.plot1.Name = "plot1";
             this.plot1.PanCursor = System.Windows.Forms.Cursors.Hand;
-            this.plot1.Size = new System.Drawing.Size(800, 446);
+            this.plot1.Size = new System.Drawing.Size(854, 465);
             this.plot1.TabIndex = 0;
             this.plot1.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
             this.plot1.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
             this.plot1.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+           
             // 
-            // button1
+            // button1_Connect
             // 
-            this.button1.Location = new System.Drawing.Point(27, 38);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1_Connect.Location = new System.Drawing.Point(12, 12);
+            this.button1_Connect.Name = "button1_Connect";
+            this.button1_Connect.Size = new System.Drawing.Size(104, 23);
+            this.button1_Connect.TabIndex = 1;
+            this.button1_Connect.Text = "Connect sensor";
+            this.button1_Connect.UseVisualStyleBackColor = true;
+            this.button1_Connect.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 112);
+            this.label1.Location = new System.Drawing.Point(12, 97);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "label1";
             // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM3";
+            // 
+            // button2_Disconnect
+            // 
+            this.button2_Disconnect.Location = new System.Drawing.Point(12, 52);
+            this.button2_Disconnect.Name = "button2_Disconnect";
+            this.button2_Disconnect.Size = new System.Drawing.Size(104, 23);
+            this.button2_Disconnect.TabIndex = 3;
+            this.button2_Disconnect.Text = "Disconnect sensor";
+            this.button2_Disconnect.UseVisualStyleBackColor = true;
+            this.button2_Disconnect.Click += new System.EventHandler(this.button2_Disconnect_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 446);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1036, 486);
+            this.Controls.Add(this.button2_Disconnect);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button1_Connect);
             this.Controls.Add(this.plot1);
             this.Name = "Form1";
             this.Text = "OxyPlot in Windows Forms";
@@ -93,7 +112,9 @@ namespace WindowsFormsDemo
         #endregion
 
         private Plot plot1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button1_Connect;
         private System.Windows.Forms.Label label1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button2_Disconnect;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@
 using System;
 using System.Windows.Forms;
 using OxyPlot;
+using System.Globalization;    
 
 namespace WindowsFormsDemo
 {
@@ -18,8 +19,32 @@ namespace WindowsFormsDemo
             var pm = new PlotModel("Trigonometric functions test", "Example using the FunctionSeries")
                          {
                              PlotType = PlotType.Cartesian,
-                             Background = OxyColors.White
+                             Background = OxyColors.White,
+                             Title = "Test"
+                       
                          };
+
+            var linearAxisX = new LinearAxis();
+            linearAxisX.Title = "Time [s]";
+            linearAxisX.Position = AxisPosition.Bottom;
+            linearAxisX.MajorGridlineColor = OxyColor.FromArgb(40, 0, 0, 139);
+            linearAxisX.MajorGridlineStyle = LineStyle.Solid;
+            linearAxisX.MinorGridlineColor = OxyColor.FromArgb(20, 0, 0, 139);
+            linearAxisX.MinorGridlineStyle = LineStyle.Solid;
+            pm.Axes.Add(linearAxisX);
+
+            var linearAxisY = new LinearAxis();
+            linearAxisY.Title = "Temperature [°C]";
+            linearAxisY.Position = AxisPosition.Left;
+            linearAxisY.MajorGridlineColor = OxyColor.FromArgb(40, 0, 0, 139);
+            linearAxisY.MajorGridlineStyle = LineStyle.Solid;
+            linearAxisY.MinorGridlineColor = OxyColor.FromArgb(20, 0, 0, 139);
+            linearAxisY.MinorGridlineStyle = LineStyle.Solid;
+            pm.Axes.Add(linearAxisY);
+
+         
+            
+
             pm.Series.Add(new FunctionSeries(Math.Sin, -10, 10, 0.1, "sin(x)"));
             pm.Series.Add(new FunctionSeries(Math.Cos, -10, 10, 0.1, "cos(x)"));
             pm.Series.Add(new FunctionSeries(t => 5 * Math.Cos(t), t => 5 * Math.Sin(t), 0, 2 * Math.PI, 0.1, "cos(t),sin(t)"));
@@ -28,6 +53,99 @@ namespace WindowsFormsDemo
 
         private void button1_Click(object sender, EventArgs e)
         {
+            serialPort1.PortName = "COM3";
+            serialPort1.BaudRate = 9600;
+            serialPort1.DtrEnable = true;
+            serialPort1.Open();
+            serialPort1.DataReceived += serialPort1_DataReceived;
+        }
+
+        private void button2_Disconnect_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            string line = serialPort1.ReadLine();
+            this.BeginInvoke(new LineReceivedEvent(LineReceived), line);
+        }
+
+        private delegate void LineReceivedEvent(string line);
+        private void LineReceived(string line)
+        {
+            double dTemperature, dTemperatureRound, dTemperatureStd;
+
+
+            try
+            {
+
+                //DateTime dtStart = DateTime.Now;
+
+                label1.Text = line; //receives line with point 21.45 to make it to double one needs to use
+                //CultureInfo.InvariantCulture
+                /*dTemperature = double.Parse(line, CultureInfo.InvariantCulture);
+                dTemperatureRound = Math.Round(dTemperature, 4);
+
+                label1.Text = Convert.ToString(dTemperatureRound);
+
+
+                chart1.Series[0].BorderWidth = 2;
+                chart1.Series[0].ChartType = SeriesChartType.Line;
+
+                tsTimespent = DateTime.Now - dtStarttime;
+                listdTimespent.Add(i);
+
+                listdTemperature.Add(dTemperatureRound);
+
+                DescriptiveStatistics descrStat = new DescriptiveStatistics(listdTemperature);
+                double dKurtosis = descrStat.Kurtosis;
+                double dSkewness = descrStat.Skewness;
+
+                dTemperatureStd = Math.Round(listdTemperature.StandardDeviation(), 4);
+                label4Std.Text = Convert.ToString(dTemperatureStd);
+                label5Mittel.Text = Convert.ToString(Math.Round(listdTemperature.Mean(), 4));
+                label6Zentral.Text = Convert.ToString(Math.Round(listdTemperature.Median(), 4));
+                label73xStd.Text = Convert.ToString(3 * dTemperatureStd);
+                label9Min.Text = Convert.ToString(Math.Round(listdTemperature.Min(), 4));
+                label9Max.Text = Convert.ToString(Math.Round(listdTemperature.Max(), 4));
+                label11Schiefe.Text = Convert.ToString(Math.Round(dSkewness, 4));
+                label11Wolbung.Text = Convert.ToString(Math.Round(dKurtosis, 4));
+
+
+
+
+                chart1.Series[0].Points.AddXY(tsTimespent.TotalSeconds, dTemperatureRound);
+                //chart1.Series[0].Points.AddXY(i, dTemperatureRound);
+
+                chart1.ChartAreas["ChartArea1"].AxisX.Minimum = listdTimespent[0];
+                chart1.ChartAreas["ChartArea1"].AxisY.Minimum = (listdTemperature.Min() - 0.5);
+                chart1.ChartAreas["ChartArea1"].AxisY.Maximum = (listdTemperature.Max() + 0.5);
+                //chart1.ChartAreas["ChartArea1"].AxisX = new Axis { LabelStyle = new LabelStyle() { Font = new Font("Verdana", 7.5f) } };
+                //chart1.ChartAreas["ChartArea1"].AxisX.TitleFont = "Verdana";
+
+                //chart1.ChartAreas["ChartArea1"].AxisX.Title = "Zeit [s]";
+                //chart1.ChartAreas["ChartArea1"].AxisY.Title = "Temparature [C]";
+
+                DateTime time = DateTime.Now;
+                //string format = "MMM ddd d HH:mm yyyy";
+                string format = "yyyyMMddHHmmssffff";
+
+                //label2.Text = time.ToString(format);
+
+                i++;
+                */
+                //comment
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
 
         }
     }
