419b3d84143d7efcbb40e2da7e44f74681a3f613
JohnnyPP <johnnnyppp@gmail.com>
2012-10-28 21:35:37 +0100

    Improvements

--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,13 @@
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
 using System;
 using System.Windows.Forms;
 using OxyPlot;
@@ -13,9 +20,24 @@ namespace WindowsFormsDemo
 {
     public partial class Form1 : Form
     {
+        string sGlobalLine;
+        double dTemperatureGlobal;
+
+        private int i = 0;
+        private DateTime dtStarttime;
+
+        private TimeSpan tsTimespent;
+
+        private List<double> listdTimespent = new List<double>();
+        private List<double> listdTemperature = new List<double>();
+
+
         public Form1()
         {
             InitializeComponent();
+
+            
+           /*
             var pm = new PlotModel("Trigonometric functions test", "Example using the FunctionSeries")
                          {
                              PlotType = PlotType.Cartesian,
@@ -42,14 +64,35 @@ namespace WindowsFormsDemo
             linearAxisY.MinorGridlineStyle = LineStyle.Solid;
             pm.Axes.Add(linearAxisY);
 
+            var lineSeries1 = new LineSeries();
+
+            lineSeries1.Color = OxyColor.FromArgb(255, 78, 154, 6);
+            lineSeries1.MarkerFill = OxyColor.FromArgb(255, 78, 154, 6);
+            lineSeries1.MarkerStroke = OxyColors.ForestGreen;
+            lineSeries1.MarkerType = MarkerType.Circle;
+            lineSeries1.StrokeThickness = 2;
+            lineSeries1.DataFieldX = "Date";
+            lineSeries1.DataFieldY = "Value";
+
+            lineSeries1.Points.Add(new DataPoint(-8,0));
+            lineSeries1.Points.Add(new DataPoint(-7.5,1));
+            lineSeries1.Points.Add(new DataPoint(-7.0,-2));
+            pm.Series.Add(lineSeries1);
+
+
+
          
             
 
-            pm.Series.Add(new FunctionSeries(Math.Sin, -10, 10, 0.1, "sin(x)"));
-            pm.Series.Add(new FunctionSeries(Math.Cos, -10, 10, 0.1, "cos(x)"));
-            pm.Series.Add(new FunctionSeries(t => 5 * Math.Cos(t), t => 5 * Math.Sin(t), 0, 2 * Math.PI, 0.1, "cos(t),sin(t)"));
+            //pm.Series.Add(new FunctionSeries(Math.Sin, -10, 10, 0.1, "sin(x)"));
+            //pm.Series.Add(new FunctionSeries(Math.Cos, -10, 10, 0.1, "cos(x)"));
+            //pm.Series.Add(new FunctionSeries(t => 5 * Math.Cos(t), t => 5 * Math.Sin(t), 0, 2 * Math.PI, 0.1, "cos(t),sin(t)"));
             plot1.Model = pm;
+             */
         }
+
+        //Observable.Interval(TimeSpan.FromSeconds(0.1)).Where( x => x < 100).Subscribe( x =>
+
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -72,6 +115,8 @@ namespace WindowsFormsDemo
         {
             string line = serialPort1.ReadLine();
             this.BeginInvoke(new LineReceivedEvent(LineReceived), line);
+            sGlobalLine = line;
+
         }
 
         private delegate void LineReceivedEvent(string line);
@@ -82,25 +127,82 @@ namespace WindowsFormsDemo
 
             try
             {
+                
 
                 //DateTime dtStart = DateTime.Now;
 
                 label1.Text = line; //receives line with point 21.45 to make it to double one needs to use
                 //CultureInfo.InvariantCulture
-                /*dTemperature = double.Parse(line, CultureInfo.InvariantCulture);
+                dTemperature = double.Parse(line, CultureInfo.InvariantCulture);
                 dTemperatureRound = Math.Round(dTemperature, 4);
 
                 label1.Text = Convert.ToString(dTemperatureRound);
+                dTemperatureGlobal = dTemperatureRound;
 
-
-                chart1.Series[0].BorderWidth = 2;
-                chart1.Series[0].ChartType = SeriesChartType.Line;
+                //chart1.Series[0].BorderWidth = 2;
+                //chart1.Series[0].ChartType = SeriesChartType.Line;
 
                 tsTimespent = DateTime.Now - dtStarttime;
                 listdTimespent.Add(i);
 
                 listdTemperature.Add(dTemperatureRound);
 
+
+                var pm = new PlotModel("Trigonometric functions test", "Example using the FunctionSeries")
+                {
+                    PlotType = PlotType.Cartesian,
+                    Background = OxyColors.White,
+                    Title = "Test"
+
+                };
+
+                var linearAxisX = new LinearAxis();
+                linearAxisX.Title = "Time [s]";
+                linearAxisX.Position = AxisPosition.Bottom;
+                linearAxisX.MajorGridlineColor = OxyColor.FromArgb(40, 0, 0, 139);
+                linearAxisX.MajorGridlineStyle = LineStyle.Solid;
+                linearAxisX.MinorGridlineColor = OxyColor.FromArgb(20, 0, 0, 139);
+                linearAxisX.MinorGridlineStyle = LineStyle.Solid;
+                pm.Axes.Add(linearAxisX);
+
+                var linearAxisY = new LinearAxis();
+                linearAxisY.Title = "Temperature [°C]";
+                linearAxisY.Position = AxisPosition.Left;
+                linearAxisY.MajorGridlineColor = OxyColor.FromArgb(40, 0, 0, 139);
+                linearAxisY.MajorGridlineStyle = LineStyle.Solid;
+                linearAxisY.MinorGridlineColor = OxyColor.FromArgb(20, 0, 0, 139);
+                linearAxisY.MinorGridlineStyle = LineStyle.Solid;
+                pm.Axes.Add(linearAxisY);
+
+                var lineSeries1 = new LineSeries();
+
+                lineSeries1.Color = OxyColor.FromArgb(255, 78, 154, 6);
+                lineSeries1.MarkerFill = OxyColor.FromArgb(255, 78, 154, 6);
+                lineSeries1.MarkerStroke = OxyColors.ForestGreen;
+                lineSeries1.MarkerType = MarkerType.Circle;
+                lineSeries1.StrokeThickness = 2;
+                lineSeries1.DataFieldX = "Date";
+                lineSeries1.DataFieldY = "Value";
+
+                lineSeries1.Points.Add(new DataPoint(i, dTemperatureRound));
+                //lineSeries1.Points.Add(new DataPoint(-7.5, 1));
+               // lineSeries1.Points.Add(new DataPoint(-7.0, -2));
+              
+                pm.Series.Add(lineSeries1);
+
+
+
+
+
+
+                //pm.Series.Add(new FunctionSeries(Math.Sin, -10, 10, 0.1, "sin(x)"));
+                //pm.Series.Add(new FunctionSeries(Math.Cos, -10, 10, 0.1, "cos(x)"));
+                //pm.Series.Add(new FunctionSeries(t => 5 * Math.Cos(t), t => 5 * Math.Sin(t), 0, 2 * Math.PI, 0.1, "cos(t),sin(t)"));
+                plot1.Model = pm;
+             
+
+
+                /*
                 DescriptiveStatistics descrStat = new DescriptiveStatistics(listdTemperature);
                 double dKurtosis = descrStat.Kurtosis;
                 double dSkewness = descrStat.Skewness;
@@ -136,9 +238,10 @@ namespace WindowsFormsDemo
 
                 //label2.Text = time.ToString(format);
 
-                i++;
+               
                 */
                 //comment
+                i++;
             }
 
             catch (Exception ex)
